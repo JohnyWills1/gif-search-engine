e3d3528e11fc7d62c35f2943b9c5756215c93ff9
Johny Wills <jonathan_wills@icloud.com>
2020-10-02 01:42:02 +0100

    fixed padding

--- components/GifItem.js
+++ components/GifItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function GifItem({ gif }) {
 	return (
 		<>
-			<li style={{ padding: '20px', float: 'left' }}>
+			<li style={{ padding: '2px', float: 'left' }}>
 				<img src={gif.images.downsized.url} />
 			</li>
 		</>
